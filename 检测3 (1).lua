f8a2694964faeb88db071d819cec0c377bb0e480
whenhwygsdvjkld45hhbdgl <3773680199@qq.com>
2026-01-23 00:04:45 +0800

    Add files via upload

---
+++ 检测3 (1).lua
@@ -0,0 +1,215 @@
+--服务器
+if game.PlaceId == 2753915549 then --BF
+
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/whenhwygsdvjkld45hhbdgl/L-l-/refs/heads/main/BF%20(2)(4).lua"))()
+
+elseif game.PlaceId == 10204250851 then --chin
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/whenhwygsdvjkld45hhbdgl/L-l-/refs/heads/main/chin.lua", true))()
+
+elseif game.PlaceId == 6839171747 then --doors
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/whenhwygsdvjkld45hhbdgl/L-l-/refs/heads/main/doors.lua", true))()
+
+elseif game.PlaceId == 93978595733734 then --暴力区
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/whenhwygsdvjkld45hhbdgl/L-l-/refs/heads/main/%E4%B8%80%E8%B7%AF%E5%90%91%E8%A5%BF1.lua", true))()
+
+elseif game.PlaceId == 18687417158 then --被遗弃
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/whenhwygsdvjkld45hhbdgl/L-l-/refs/heads/main/".."被遗弃.lua", true))()
+
+elseif game.PlaceId == 119048529960596 then --餐厅大亨
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/whenhwygsdvjkld45hhbdgl/L-l-/refs/heads/main/".."餐厅大亨.lua", true))()
+
+elseif game.PlaceId == 119574637420814 then --成为乞丐
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/whenhwygsdvjkld45hhbdgl/L-l-/refs/heads/main/".."成为乞丐.lua", true))()
+
+elseif game.PlaceId == 13772394625 then --刀刃球
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/whenhwygsdvjkld45hhbdgl/L-l-/refs/heads/main/".."刀刃球.lua", true))()
+
+elseif game.PlaceId == 9872472334 then --躲避
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/whenhwygsdvjkld45hhbdgl/L-l-/refs/heads/main/".."躲避.lua", true))()
+
+elseif game.PlaceId == 13822889 then --伐木大亨
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/whenhwygsdvjkld45hhbdgl/L-l-/refs/heads/main/".."伐木大亨.lua", true))()
+
+elseif game.PlaceId == 4618049391 then --河北唐县
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/whenhwygsdvjkld45hhbdgl/L-l-/refs/heads/main/".."河北唐县.lua", true))()
+
+elseif game.PlaceId == 3101667897 then --极速传奇
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/whenhwygsdvjkld45hhbdgl/L-l-/refs/heads/main/".."极速传奇.lua", true))()
+
+elseif game.PlaceId == 155615604 then --监狱人生
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/whenhwygsdvjkld45hhbdgl/L-l-/refs/heads/main/".."监狱人生.lua", true))()
+
+elseif game.PlaceId == 17362847861 then --建造一架飞机
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/whenhwygsdvjkld45hhbdgl/L-l-/refs/heads/main/".."建造一架飞机.lua", true))()
+
+elseif game.PlaceId == 7711635737 then --紧急汉堡
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/whenhwygsdvjkld45hhbdgl/L-l-/refs/heads/main/".."紧急汉堡.lua", true))()
+
+elseif game.PlaceId == 4110892437 then --开战宇宙
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/whenhwygsdvjkld45hhbdgl/L-l-/refs/heads/main/".."开战宇宙.lua", true))()
+
+elseif game.PlaceId == 112279762578792 then --矿井
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/whenhwygsdvjkld45hhbdgl/L-l-/refs/heads/main/".."矿井.lua", true))()
+
+elseif game.PlaceId == 3623096087 then --力量传奇
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/whenhwygsdvjkld45hhbdgl/L-l-/refs/heads/main/".."力量传奇.lua", true))()
+
+elseif game.PlaceId == 123701320605975 then --脑叶公司
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/whenhwygsdvjkld45hhbdgl/L-l-/refs/heads/main/".."脑叶公司.lua", true))()
+
+elseif game.PlaceId == 138878871969281 then --皮肤信奉者
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/whenhwygsdvjkld45hhbdgl/L-l-/refs/heads/main/".."皮肤信奉者.lua", true))()
+
+elseif game.PlaceId == 2248408710 then --破坏模拟器
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/whenhwygsdvjkld45hhbdgl/L-l-/refs/heads/main/".."破坏模拟器.lua", true))()
+
+elseif game.PlaceId == 122160128185618 then --启示录
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/whenhwygsdvjkld45hhbdgl/L-l-/refs/heads/main/".."启示录.lua", true))()
+
+elseif game.PlaceId == 1554960397 then --汽车经销大亨
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/whenhwygsdvjkld45hhbdgl/L-l-/refs/heads/main/".."汽车经销大亨.lua", true))()
+
+elseif game.PlaceId == 14518422161 then --枪战竞技场
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/whenhwygsdvjkld45hhbdgl/L-l-/refs/heads/main/".."枪战竞技场.lua", true))()
+
+elseif game.PlaceId == 8737602449 then --请捐赠
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/whenhwygsdvjkld45hhbdgl/L-l-/refs/heads/main/".."请捐赠.lua", true))()
+
+elseif game.PlaceId == 79546208627805 then --森林中的99夜
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/whenhwygsdvjkld45hhbdgl/L-l-/refs/heads/main/".."森林中的99页.lua", true))()
+
+elseif game.PlaceId == 70876832253163 then --死铁轨
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/whenhwygsdvjkld45hhbdgl/L-l-/refs/heads/main/".."死铁轨.lua", true))()
+
+elseif game.PlaceId == 83678792452277 then --死亡球
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/whenhwygsdvjkld45hhbdgl/L-l-/refs/heads/main/".."死亡球.lua", true))()
+
+elseif game.PlaceId == 1345139196 then --寻宝模拟器
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/whenhwygsdvjkld45hhbdgl/L-l-/refs/heads/main/".."寻宝模拟器(1).lua", true))()
+
+elseif game.PlaceId == 2474168535 then --一路向西
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/whenhwygsdvjkld45hhbdgl/L-l-/refs/heads/main/".."一路向西1.lua", true))()
+
+elseif game.PlaceId == 4282985734 then --战斗勇士
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/whenhwygsdvjkld45hhbdgl/L-l-/refs/heads/main/".."战斗勇士.lua", true))()
+
+elseif game.PlaceId == 4639625707 then --战争大亨
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/whenhwygsdvjkld45hhbdgl/L-l-/refs/heads/main/".."战争大亨.lua", true))()
+
+elseif game.PlaceId == 14888386963 then --殖民地
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/whenhwygsdvjkld45hhbdgl/L-l-/refs/heads/main/".."殖民地.lua", true))()
+
+elseif game.PlaceId == 11815767793 then --终极战场
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/whenhwygsdvjkld45hhbdgl/L-l-/refs/heads/main/".."终极战场.lua", true))()
+
+elseif game.PlaceId == 98108870853109 then --穷小子打工记
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/whenhwygsdvjkld45hhbdgl/L-l-/refs/heads/main/".."穷小子打工记.lua", true))()
+
+elseif game.PlaceId == 7991339063 then --彩虹朋友
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/whenhwygsdvjkld45hhbdgl/L-l-/refs/heads/main/".."彩虹朋友.lua", true))()
+
+elseif game.PlaceId == 654732683 then --汽车破坏
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/whenhwygsdvjkld45hhbdgl/L-l-/refs/heads/main/".."汽车破坏(1).lua", true))()
+
+elseif game.PlaceId == 3652625463 then --提升模拟器
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/whenhwygsdvjkld45hhbdgl/L-l-/refs/heads/main/".."提升模拟器.lua", true))()
+
+elseif game.PlaceId == 6766156863 then --超级大力士模拟器
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/whenhwygsdvjkld45hhbdgl/L-l-/refs/heads/main/".."超级大力士模拟器.lua", true))()
+
+elseif game.PlaceId == 1693323219 then --蜂群模拟器
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/whenhwygsdvjkld45hhbdgl/L-l-/refs/heads/main/".."蜂群模拟器.lua", true))()
+
+elseif game.PlaceId == 5256165620 then --火箭发射器
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/whenhwygsdvjkld45hhbdgl/L-l-/refs/heads/main/".."火箭发射模拟器.lua", true))()
+
+elseif game.PlaceId == 16480898254 then --吃掉世界
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/whenhwygsdvjkld45hhbdgl/L-l-/refs/heads/main/".."吃掉世界.lua", true))()
+
+elseif game.PlaceId == 662417684 then --幸运方块
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/whenhwygsdvjkld45hhbdgl/L-l-/refs/heads/main/".."幸运方块.lua", true))()
+
+elseif game.PlaceId == 121418861436763 then --在超市生活一周
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/whenhwygsdvjkld45hhbdgl/L-l-/refs/heads/main/".."在超市生活一周(3).lua", true))()
+
+elseif game.PlaceId == 12552538292 then --压力
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/whenhwygsdvjkld45hhbdgl/L-l-/refs/heads/main/".."压力.lua", true))()
+
+elseif game.PlaceId == 5985232436 then --感染性微笑
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/whenhwygsdvjkld45hhbdgl/L-l-/refs/heads/main/".."感染的微笑.lua", true))()
+
+elseif game.PlaceId == 1390601379 then --恶魔学
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/whenhwygsdvjkld45hhbdgl/L-l-/refs/heads/main/".."恶魔学(1).lua", true))()
+
+elseif game.PlaceId == 76455837887178 then --digit
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/whenhwygsdvjkld45hhbdgl/L-l-/refs/heads/main/".."Dig it(2).lua", true))()
+
+elseif game.PlaceId == 10253248401 then --元素力量大亨
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/whenhwygsdvjkld45hhbdgl/L-l-/refs/heads/main/".."元素力量大亨.lua", true))()
+
+elseif game.PlaceId == 80898524797320 then --画我
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/whenhwygsdvjkld45hhbdgl/L-l-/refs/heads/main/".."画我.lua", true))()
+
+elseif game.PlaceId == 2210085102 then --海战
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/whenhwygsdvjkld45hhbdgl/L-l-/refs/heads/main/".."海战.lua", true))()
+
+
+elseif game.PlaceId == 124977557560410 then --种植花园
+
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/whenhwygsdvjkld45hhbdgl/L-l-/refs/heads/main/".."种植花园.lua"))()
+
+	
+else --通用
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/whenhwygsdvjkld45hhbdgl/L-l-/refs/heads/main/%E5%A4%A7%E5%8F%B8%E9%A9%AC%E4%BB%98%E8%B4%B9%E9%80%9A%E7%94%A8(2).lua"))()
+    
+end
